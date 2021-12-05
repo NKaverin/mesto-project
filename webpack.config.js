75252a2f1d6f6ea15f6a73c17141ac315ca22a62
NKaverin <87134966+NKaverin@users.noreply.github.com>
2021-12-05 16:18:54 +0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,6 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const html-loader= require('html-loader');
 
 module.exports = {
   entry: {
